263710c2d8e36d4d32c93316adef320f9fa58047
Diaz <alejandro.a.diaz@accenture.com>
2019-10-02 18:11:23 -0500

    first commit

---
+++ src/main/java/com/walmart/rpa/RPAServicesApplication.java
@@ -0,0 +1,13 @@
+package com.walmart.rpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RPAServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RPAServicesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/walmart/rpa/config/SwaggerDocumentationConfig.java
@@ -0,0 +1,28 @@
+package com.walmart.rpa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerDocumentationConfig {
+	ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("Walmart API").description("API for walmart").license("")
+				.licenseUrl("http://unlicense.org").termsOfServiceUrl("").version("0.0.1")
+				.contact(new Contact("", "", "")).build();
+	}
+
+	@Bean
+	public Docket customImplementation() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.formacionbdi.springboot.app.controllers")).build()
+				.useDefaultResponseMessages(false)
+				.directModelSubstitute(org.threeten.bp.LocalDate.class, java.sql.Date.class)
+				.directModelSubstitute(org.threeten.bp.OffsetDateTime.class, java.util.Date.class).apiInfo(apiInfo());
+	}
+}
